c8691f6895abeb824aeca6b43c29f7df6b8bbacc
8Rudy8 <rudy24.morello@gmail.com>
2025-06-06 11:19:42 +0200

    Update SymbolicFact.jl
    Adding a way to get the post-order from the result of symbolicfact

--- src/SymbolicFact.jl
+++ src/SymbolicFact.jl
@@ -40,11 +40,13 @@ module SymbolicFact
 - `rc::Int`: The **row count** of the considered node, equal `0` if the node is a subnode.
 - `cc:Int`: The **column count** of the considered node, equal `-1` if there is no column count.
 - `idxr::Set`: The **row indices** of the left column of the front matrix. Equal `Set([])` if the node considered is a subnode.
-- `idxc::Set`: Not implemented
+- `idxc::Set`:
+- `np::Int`: the supernode size, equal 0 if the node is a subnode
+- `porder::Int`: the post-order of the node
 
 ### See also
-- [`etree`](@ref)
-- [`rowcolcount!`](@ref)
+- [`etree`](@link)
+- [`rowcolcount!`](@link)
 """
 struct Snode
 	father ::Int 
@@ -53,9 +55,10 @@ struct Snode
 	idxr::Set
 	idxc::Set
 	np::Int
+	porder::Int
 	#defining constructors
-	Snode(p,x,y,z,t,e) = new(p,x,y,z,t,e) #with all params
-	Snode() = new(0,0,0,Set(),Set(),1)#empty node
+	Snode(p,x,y,z,t,e,a) = new(p,x,y,z,t,e,a) #with all params
+	Snode() = new(0,0,0,Set(),Set(),1,0)#empty node
 end
 """
 	symbolicfact(A[,supernodes = true])
@@ -69,15 +72,9 @@ Make the symbolic factorization of `A`.
 ## Return
 Return a `Vector` containing the symbolic factorization informations. Each element of it is an instance of [`Snode`](@link) representing a column.
 
-### note
-This function does not include fill reducing permutations.
-
 ### See also
-- [`etree`](@ref)
-- [`Snode`](@ref)
-- [`rowcolcount!`](@ref)
-- [`supernodecount`](@ref)
-- [`idx_r`](@ref)
+- [`Snode`](@link)
+- [`rowcolcount!`](@link)
 """
 function symbolicfact(A,supernodes = true)
 	elimtree = etree(A)
@@ -91,14 +88,14 @@ function symbolicfact(A,supernodes = true)
 		for i = 1:n
 			#we set the column count to -1 to make difference beetween
 			#no column count and column count equal 0
-			result[i] = Snode(elimtree[i],rowcount[i],-1,idxr[i],Set(),np[i])
+			result[i] = Snode(elimtree[i],rowcount[i],-1,idxr[i],Set(),np[i],porder[i])
 		end
 	else
 		rowcount,colcount = rowcolcount!(A,elimtree,porder,supernodes)
 		np = supernodecount(elimtree)
 		#idxr = idx_r(B,elimtree,porder) #not implemented yet for a non-symmetric matrix
 		for i = 1:n
-			result[i] = Snode(elimtree[i],rowcount[i],colcount[i], Set(),Set(),np[i])
+			result[i] = Snode(elimtree[i],rowcount[i],colcount[i], Set(),Set(),np[i],porder[i])
 		end
 	end
 	return result
